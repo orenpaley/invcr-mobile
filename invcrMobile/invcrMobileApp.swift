dd537153d8fd519063c33fb7cf66944c9275f302
Oren Paley <oepaley@gmail.com>
2024-04-19 12:57:24 -0500

    Initial Commit

---
+++ invcrMobile/invcrMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  invcrMobileApp.swift
+//  invcrMobile
+//
+//  Created by Oren Paley on 4/19/24.
+//
+
+import SwiftUI
+
+@main
+struct invcrMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
