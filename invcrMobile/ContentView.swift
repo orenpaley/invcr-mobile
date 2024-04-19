fa7401e5bdada2243f8712e6b52b6aefc3c9c031
Oren Paley <oepaley@gmail.com>
2024-04-19 13:36:18 -0500

    changed project folder to invcrMobile

--- invcrMobile/ContentView.swift
+++ invcrMobile/ContentView.swift
@@ -1,24 +1,40 @@
 //
 //  ContentView.swift
-//  invcrMobile
 //
-//  Created by Oren Paley on 4/19/24.
+//  Created by Oren Paley on 4/16/24.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+   
+    @StateObject var invoice = Invoice()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color(red: 0.4627, green: 0.8392, blue: 1.0)
+                .opacity(0.5)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Spacer()
+                Text("invcr").font(.largeTitle)
+                Spacer()
+                Text("create a saveable PDF of your invoice").font(.headline)
+                Text("with ease").font(.title).foregroundColor(.pink).opacity(0.7)
+                    .italic()
+                Spacer()
+                NavigationLink("make invoice", destination: invcr())
+                    .foregroundColor(.blue)
+                    .padding()
+                    .font(.title)
+                    .border(Color.blue)
+                    .background()
+                Spacer()
+            }
         }
-        .padding()
+       
     }
 }
-
+ 
 #Preview {
     ContentView()
 }
